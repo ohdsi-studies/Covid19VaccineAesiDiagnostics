2091fd249a6ae74e6f885549513dc07eb4feafd6
Gowtham Rao <rao@ohdsi.org>
2021-06-29 12:48:52 -0400

    add example script

---
+++ extras/exampleCodeToRun/dataSourceInformation.R
@@ -0,0 +1,170 @@
+ROhdsiWebApi::authorizeWebApi(Sys.getenv('baseUrl'), "windows") # Windows authentication - if security enabled using windows authentication
+
+
+cdmSources <-
+  ROhdsiWebApi::getCdmSources(baseUrl = Sys.getenv('baseUrl')) %>%
+  dplyr::mutate(
+    baseUrl = Sys.getenv('baseUrl'),
+    dbms = 'redshift',
+    sourceDialect = 'redshift',
+    port = 5439,
+    version = .data$sourceKey %>% substr(., nchar(.) - 3, nchar(.)) %>% as.integer(),
+    database = .data$sourceKey %>% substr(., 5, nchar(.) - 6)
+  ) %>%
+  dplyr::group_by(.data$database) %>%
+  dplyr::arrange(dplyr::desc(.data$version)) %>%
+  dplyr::mutate(sequence = dplyr::row_number()) %>%
+  dplyr::ungroup() %>%
+  dplyr::arrange(.data$database, .data$sequence) %>%
+  dplyr::mutate(server = tolower(paste0(
+    Sys.getenv("serverRoot"), "/", .data$database
+  ))) %>%
+  dplyr::mutate(cohortDatabaseSchema = paste0("scratch_", keyring::key_get(service = keyringUserService))) %>%
+  dplyr::filter(database %in% databaseIds) %>%
+  dplyr::filter(sequence == 1)
+
+
+
+######
+execute <- function(x) {
+  if (x$generateCohortTableName) {
+    cohortTableName <- paste0(
+      stringr::str_squish(x$databaseId),
+      stringr::str_squish('SkeletonCohortDiagnosticsStudy')
+    )
+  }
+  
+  # this function gets details of the data source from cdm source table in omop, if populated.
+  # The assumption is the cdm_source.sourceDescription has text description of data source.
+  getDataSourceDetails <-
+    function(connection,
+             databaseId,
+             cdmDatabaseSchema) {
+      sqlCdmDataSource <- "select * from @cdmDatabaseSchema.cdm_source;"
+      sourceInfo <- list(cdmSourceName = databaseId,
+                         sourceDescription = databaseId)
+      tryCatch(
+        expr = {
+          cdmDataSource <-
+            DatabaseConnector::renderTranslateQuerySql(
+              connection = connection,
+              sql = sqlCdmDataSource,
+              cdmDatabaseSchema = cdmDatabaseSchema,
+              snakeCaseToCamelCase = TRUE
+            )
+          if (nrow(cdmDataSource) == 0) {
+            return(sourceInfo)
+          }
+          if (sourceDescription %in% colnames(cdmDataSource)) {
+            sourceInfo$sourceDescription <- cdmDataSource$sourceDescription
+          }
+          if (cdmSourceName %in% colnames(cdmDataSource)) {
+            sourceInfo$cdmSourceName <- cdmDataSource$cdmSourceName
+          }
+        },
+        error = function(...) {
+          return(sourceInfo)
+        }
+      )
+      return(sourceInfo)
+    }
+  
+  # Details for connecting to the server:
+  connectionDetails <-
+    DatabaseConnector::createConnectionDetails(
+      dbms = x$cdmSource$dbms,
+      server = x$cdmSource$server,
+      user = keyring::key_get(service = x$userService),
+      password =  keyring::key_get(service = x$passwordService),
+      port = x$cdmSource$port
+    )
+  # The name of the database schema where the CDM data can be found:
+  cdmDatabaseSchema <- x$cdmSource$cdmDatabaseSchema
+  cohortDatabaseSchema <- x$cdmSource$cohortDatabaseSchema
+  
+  connection <-
+    DatabaseConnector::connect(connectionDetails = connectionDetails)
+  dataSourceDetails <- getDataSourceDetails(
+    connection = connection,
+    databaseId = x$databaseId,
+    cdmDatabaseSchema = cdmDatabaseSchema
+  )
+  DatabaseConnector::disconnect(connection)
+  
+  SkeletonCohortDiagnosticsStudy::execute(
+    connectionDetails = connectionDetails,
+    cdmDatabaseSchema = cdmDatabaseSchema,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTableName,
+    verifyDependencies = x$verifyDependencies,
+    outputFolder = x$outputFolder,
+    databaseId = x$databaseId,
+    databaseName = dataSourceDetails$databaseName,
+    databaseDescription = dataSourceDetails$databaseDescription
+  )
+  
+  if (x$preMergeDiagnosticsFiles) {
+    CohortDiagnostics::preMergeDiagnosticsFiles(dataFolder = x$outputFolder)
+  }
+  
+  if (length(x$privateKeyFileName) > 0 && 
+      !x$privateKeyFileName == '' &&
+      length(x$userName) > 0 &&
+      !x$userName == '') {
+    CohortDiagnostics::uploadResults(x$outputFolder,
+                                     x$privateKeyFileName,
+                                     x$userName)
+  }
+  
+  if (length(x$uploadToLocalPostGresDatabaseSpecifications) > 1) {
+    # Set the POSTGRES_PATH environmental variable to the path to the folder containing the psql executable to enable bulk upload (recommended).
+    
+    connectionPostGres <- DatabaseConnector::connect(x$uploadToLocalPostGresDatabaseSpecifications$connectionDetails)
+    
+    # check if schema was instantiated
+    sqlSchemaCheck <-
+      paste0(
+        "SELECT * FROM information_schema.schemata WHERE schema_name = '",
+        tolower(x$uploadToLocalPostGresDatabaseSpecifications$schema),
+        "';"
+      )
+    schemaExists <-
+      DatabaseConnector::renderTranslateQuerySql(
+        connection = connectionPostGres,
+        sql = sqlSchemaCheck
+      )
+    
+    if (nrow(schemaExists) == 0) {
+      warning(
+        paste0(
+          'While attempting to upload to postgres, found target schema to not exist - attempting to create target schema ',
+          x$uploadToLocalPostGresDatabaseSpecifications$schema
+        )
+      )
+      createSchemaSql <-
+        paste0(
+          "select create_schema('",
+          tolower(x$uploadToLocalPostGresDatabaseSpecifications$schema),
+          ");"
+        )
+      DatabaseConnector::renderTranslateQuerySql(
+        connection = connectionPostGres,
+        sql = createSchemaSql
+      )
+      ParallelLogger::logInfo("Schema created.")
+      
+    }
+    # check if required table exists, else create them
+    if (!DatabaseConnector::dbExistsTable(conn = connectionPostGres, name = 'cohort_count')) {
+      CohortDiagnostics::createResultsDataModel(connection = connectionPostGres, schema = tolower(x$uploadToLocalPostGresDatabaseSpecifications$schema))
+    }
+    
+    DatabaseConnector::disconnect(connection = connectionPostGres)
+    # note this is a thread safe upload, so its ok to parallelize
+    CohortDiagnostics::uploadResults(
+      connectionDetails = x$uploadToLocalPostGresDatabaseSpecifications$connectionDetails,
+      schema = x$uploadToLocalPostGresDatabaseSpecifications$schema,
+      zipFileName = x$uploadToLocalPostGresDatabaseSpecifications$zipFileName
+    )
+  }
+}
---
+++ extras/exampleCodeToRun/exampleRedShiftParallell.R
@@ -0,0 +1,153 @@
+############### Note this is a custom script, a version of CodeToRun.R that may not work for everyone ##############
+############### Please use CodeToRun.R as it a more generic version  ###############################################
+
+############### This version of code will run cohort diagnostics in parallel                      ##################
+
+############### processes to run Cohort Diagnostics ################################################################
+library(magrittr)
+
+################################################################################
+# VARIABLES - please change
+################################################################################
+# The folder where the study intermediate and result files will be written:
+outputFolder <- "D:/studyResults/Covid19VaccineAesiDiagnostics"
+# create output directory if it does not exist
+if (!dir.exists(outputFolder)) {
+  dir.create(outputFolder,
+             showWarnings = FALSE,
+             recursive = TRUE)
+}
+# Optional: specify a location on your disk drive that has sufficient space.
+# options(andromedaTempFolder = "s:/andromedaTemp")
+
+# set to false if email is not possible
+mailFatal <- TRUE
+
+# do you want to upload the results to a local database
+uploadToLocalPostGresDatabase <- TRUE
+
+############## databaseIds to run cohort diagnostics on that source  #################
+databaseIds <-
+  c(
+    'truven_ccae',
+    'truven_mdcd',
+    'truven_mdcr',
+    'cprd',
+    'jmdc',
+    'optum_extended_dod',
+    'optum_ehr',
+    'ims_australia_lpd',
+    'ims_germany',
+    'ims_france'
+  )
+
+## service name for keyring for db with cdm
+keyringUserService <- 'OHDSI_USER'
+keyringPasswordService <- 'OHDSI_PASSWORD'
+
+## service name for keyring for postgres db to upload results
+keyringUserServicePostGresUpload <- 'shinydbUser'
+keyringPasswordServicePostGresUpload <- 'shinydbPW'
+keyringDatabaseServicePostGresUpload <- 'shinydbDatabase'
+keyringServerServicePostGresUpload <- 'shinydbServer'
+keyringPortServicePostGresUpload <- 'shinydbPort'
+
+# lets get meta information for each of these databaseId. This includes connection information.
+source("extras/examplesOfCodeToRun/dataSourceInformation.R")
+
+## if uploading to co-ordinator site
+privateKeyFileName <- ""
+siteUserName <- ""
+
+###### create a list object that contain connection and meta information for each data source
+x <- list()
+for (i in (1:length(databaseIds))) {
+  databaseId <- databaseIds[[i]]
+  cdmSource <- cdmSources %>%
+    dplyr::filter(database == databaseId)
+  
+  if (uploadToLocalPostGresDatabase) {
+    uploadToLocalPostGresDatabaseSpecifications <- list(
+      connectionDetails = DatabaseConnector::createConnectionDetails(
+        dbms = "postgresql",
+        server = paste(
+          keyring::key_get(keyringServerServicePostGresUpload),
+          keyring::key_get(keyringDatabaseServicePostGresUpload),
+          sep = "/"
+        ),
+        port = keyring::key_get(keyringPortServicePostGresUpload),
+        user = keyring::key_get(keyringUserServicePostGresUpload),
+        password = keyring::key_get(keyringPasswordServicePostGresUpload)
+      ),
+      schema = 'SkeletonCohortDiagnosticsStudy',
+      zipFileName = list.files(
+        path = file.path(outputFolder, databaseId),
+        pattern = paste0("Results_", databaseId, ".zip"),
+        full.names = TRUE,
+        recursive = TRUE
+      )
+    )
+  } else {
+    uploadToLocalPostGresDatabaseSpecifications <- ''
+  }
+  
+  
+  x[[i]] <- list(
+    cdmSource = cdmSource,
+    generateCohortTableName = TRUE,
+    verifyDependencies = TRUE,
+    databaseId = databaseId,
+    outputFolder = file.path(outputFolder, databaseId),
+    userService = keyringUserService,
+    passwordService = keyringPasswordService,
+    preMergeDiagnosticsFiles = TRUE,
+    privateKeyFileName = privateKeyFileName,
+    userName = siteUserName,
+    uploadToLocalPostGresDatabaseSpecifications = uploadToLocalPostGresDatabaseSpecifications
+  )
+}
+
+# x <- x[9:10]
+
+# use Parallel Logger to run in parallel
+cluster <-
+  ParallelLogger::makeCluster(numberOfThreads = as.integer(trunc(parallel::detectCores() /
+                                                                   2)))
+
+## file logger
+loggerName <-
+  paste0(
+    "CDF_",
+    stringr::str_replace_all(
+      string = Sys.time(),
+      pattern = ":|-|EDT| ",
+      replacement = ''
+    )
+  )
+loggerTrace <-
+  ParallelLogger::addDefaultFileLogger(fileName = paste0(loggerName, ".txt"))
+
+## fatal email logger
+if (mailFatal) {
+  mailSettings <- list(
+    from = keyring::key_get("mailAddress"),
+    to = c(keyring::key_get("mailToAddress")),
+    smtp = list(
+      host.name = keyring::key_get("mailSmtpServer"),
+      port = keyring::key_get("mailSmtpPort"),
+      user.name = keyring::key_get("mailAddress"),
+      passwd = keyring::key_get("mailPassword"),
+      ssl = TRUE
+    ),
+    authenticate = TRUE,
+    send = TRUE
+  )
+  ParallelLogger::addDefaultEmailLogger(mailSettings = mailSettings, label = Sys.info()["nodename"])
+}
+
+ParallelLogger::clusterApply(cluster = cluster,
+                             x = x,
+                             fun = execute)
+
+writeLines(readChar(paste0(loggerName, ".txt"), file.info(paste0(loggerName, ".txt"))$size))
+ParallelLogger::stopCluster(cluster = cluster)
